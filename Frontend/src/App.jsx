7265f3998a56a086f8bdd37a2e881c0ab0fd8401
samlan24 <muchemionline@gmail.com>
2024-12-13 15:40:22 +0300

    adding spleeting instrumentals from vocals frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AnalyzePage from './components/Analyze';
 import ChangeBpmPage from './components/Change';
 import ConvertAudio from './components/Convert';
+import AudioSeparator from './components/Spleet';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path="/analyze" element={<AnalyzePage />} />
         <Route exact path="/change" element={<ChangeBpmPage />} />
         <Route exact path="/convert" element={<ConvertAudio />} />
+        <Route exact path="/spleet" element={<AudioSeparator />} />
       </Routes>
     </Router>
   );
